47cb90fb5becc7a75ca34785335671dbf5e542b9
Sara Hunter <34786124+sarashunter@users.noreply.github.com>
2018-07-21 10:03:19 -0400

    Add character display function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,13 +6,8 @@ $(document).ready(function () {
         this.ap = ap;
         this.cap = cap;
         this.isDefender = false;
-
-        this.makeDefender = function () {
-            this.isDefender = true;
-            game.hasDefenderBeenChosen = true;
-            console.log("The defender is " + this.name);
-            console.log("Has the defender been chosen " + game.hasDefenderBeenChosen);
-            game.defender=this.name;
+        this.displayHTML = function(){
+            return "<img src ='assets/images/" + this.name + ".jpg'>";
         }
     }
 
@@ -21,11 +16,12 @@ $(document).ready(function () {
         initialize: function (winsSoFar, lossesSoFar) {
             this.wins = winsSoFar;
             this.losses = lossesSoFar;
+            this.defender = "me";
             console.log("Wins so far: " + winsSoFar);
             console.log("Losses so far " + lossesSoFar);
             this.hasDefenderBeenChosen = false;
-            // dog = Character("dog", 50, 5, 10);
-            // cat = Character("cat", 90, 10, 15);
+            // character1 = Character("character1", 50, 5, 10);
+            // character2 = Character("character2", 90, 10, 15);
         }
 
     }
@@ -35,28 +31,52 @@ $(document).ready(function () {
         defender.ap = defender.ap + defender.ap;
 
     }
+    function chooseDefender(character) {
 
+        character.isDefender = true;
+        game.hasDefenderBeenChosen = true;
+        game.defender = character;
+
+        console.log("The defender is " + game.defender.name);
+        console.log("Has the defender been chosen " + game.hasDefenderBeenChosen);
+
+        //Display the defender at the bottom.
+        $("#defenderdiv").html(character.displayHTML());
+
+    }
+    
+    function displayEnemies(){
+        for(var i=0; i<4; i++){
+            //this function should display the enemies in the enemy field.  It will be called after the defender is chosen.
+            //It will also clear them from the top and remove the text saying to choose a defender.
+        }
+    }
 
     //Create objects for each character.
-    var dog = new Character("dog", 40, 5, 5);
-    var cat = new Character("cat", 30, 6, 6);
-    console.log(dog);
-    // var cat = new character;
+    var character1 = new Character("character1", 40, 5, 5);
+    var character2 = new Character("character2", 30, 6, 6);
+    console.log(character1);
+    // var character2 = new character;
     //Initialize the game with a clear record.
     game.initialize(0, 0);
 
     $("#1").on("click", function () {
-        if (!game.defenderChosen) {
-            dog.makeDefender();
-            console.log("The games defender is " + game.defenderChosen);
+        if (!game.hasDefenderBeenChosen) {
+            chooseDefender(character1);
         }
     });
+
+    $("#2").on("click", function () {
+        if (!game.hasDefenderBeenChosen) {
+            chooseDefender(character2);
+        }
+    });
+
     console.log("hello" + game.hasDefenderBeenChosen);
-    $("#2").on("click", function () { cat.makeDefender() });
 
 
-console.log(dog);
-//     $("#1").on("click", fight(dog, cat));
-// fight(dog, cat);
+    console.log(character1);
+    //     $("#1").on("click", fight(character1, character2));
+    // fight(character1, character2);
 
 })
